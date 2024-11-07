4a72ad255a15b5bcba39e98be8b3ca662e2087d3
Ethan Chapman <ethan.chapman0@outlook.com>
2024-11-06 23:47:17 -0700

    watchdog timer sucks

--- main/src/main.rs
+++ main/src/main.rs
@@ -5,6 +5,7 @@ extern crate msp432P401r_api;
 extern crate cortex_m_rt;
 extern crate panic_halt;
 
+use core::arch::asm;
 use cortex_m_rt::entry;
 
 use msp432P401r_api::Dio;
@@ -16,14 +17,22 @@ use panic_halt as _;
 #[entry]
 unsafe fn main() -> ! {
     let p = msp432P401r_api::Peripherals::steal();
+
+    p.wdt_a.wdtctl().write( |w| { w.bits(0x5A80) });
     
     let dio = p.dio;
     
     init_led(&dio);
     
-    set_led(&dio, GREEN | BLUE);
-    
-    loop {}
+    loop {
+        set_led(&dio, RED);
+
+        delay(1000000);
+
+        set_led(&dio, BLUE);
+
+        delay(1000000);
+    }
 }
 
 fn init_led(dio: &Dio) {
@@ -36,4 +45,12 @@ const BLUE: u8 = 0b100;
 
 fn set_led(dio: &Dio, color: u8) {
     dio.paout().modify(|r, w| unsafe { w.p2out().bits((r.p2out().bits() & !0b111) | color) });
+}
+
+
+extern "C" fn delay(count: u64){
+    unsafe {
+        asm!( "2:  subs    r0, #1",
+            "    bne    2b");
+    }
 }
